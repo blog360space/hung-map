8649663ba0d38d4b4bd847d861405a77dde2fd4b
Hung Tran <hung.tran@seldatinc.com>
2017-02-11 16:19:51 +0700

    Add intervention/image, update sizes avaiable

--- resources/views/upload/index.blade.php
+++ resources/views/upload/index.blade.php
@@ -11,11 +11,10 @@
 
         <div class="form-group text-center">
         <p>{!! Form::file('file', ['multiple'=>true, 'class' => 'form-control']) !!}</p>
-        <p class="text-left">
-            Thumbs: <br/>
-            <label for="rs-sm">Sm</label> {{ Form::checkbox('resize[]', 'sm', true, ['id' => 'rs-sm']) }}
-            <label for="rs-md">Md</label> {{ Form::checkbox('resize[]', 'md', true, ['id' => 'rs-md']) }}
-            <label for="rs-lg">Lg</label> {{ Form::checkbox('resize[]', 'lg', true, ['id' => 'rs-lg']) }}
+        <p class="text-left">            
+            {{ Form::checkbox('resize[]', 'sm', true, ['id' => 'rs-sq']) }}<label for="rs-sq" title="Square 150x150">Square</label><br />
+            {{ Form::checkbox('resize[]', 'sm', true, ['id' => 'rs-sm']) }}<label for="rs-sm" title="Small 320x214">Small</label><br />
+            {{ Form::checkbox('resize[]', 'md', true, ['id' => 'rs-md']) }}<label for="rs-md" title="Medium 640x428">Medium</label><br />            
         </p>
         <p>
         {!! Form::submit('Save', 
@@ -27,23 +26,20 @@
     @if (count($files) > 0)
     @foreach ($files as $file)
     <div class="col-sm-3 col-xs-3 col-md-2">
-      <div class="thumbnail hideOverflow">
-          <img src="{{ url('/files/' . $endpoint . '/' . $file)  }}" title="{{ $file }}" alt="{{ $file }}">
-          
-      </div>
+        <div class="thumbnail hideOverflow">
+            <img src="{{ url('/files/' . $endpoint . '/' . $file)  }}" title="{{ $file }}" alt="{{ $file }}">
+        </div>
         <div class="caption">
-          
-              <form action="{{url('upload/' .$endpoint)}}" method="POST">
-                    {{ csrf_field() }}
-                    {{ method_field('DELETE') }}
-                    {{ Form::hidden('fileName', $file) }} 
+            <form action="{{url('upload/' .$endpoint)}}" method="POST">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                {{ Form::hidden('fileName', $file) }} 
 
-                    <button type="submit" class="btn btn-danger btn-xs">
-                        <i class="fa fa-btn fa-trash"></i>Delete
-                    </button>
-                </form>
-          
-            </div>
+                <button type="submit" class="btn btn-danger btn-xs">
+                    <i class="fa fa-btn fa-trash"></i>Delete
+                </button>
+            </form>
+        </div>
     </div>
     @endforeach
 </div>
